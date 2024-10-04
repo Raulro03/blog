9a66253a084691059b1266e2ff99bff3ac3f1a36
Raulro03 <raulromeraortiz@gmail.com>
2024-10-04 17:10:17 +0200

    medio paso 4

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,6 @@
 <x-layout meta-title="Blog" meta-description="Descripcion de la pagina de Blog">
     <h1>Blog</h1>
-
+    @foreach($posts as $post)
+    <h2>{{ $post['title'] }}</h2>
+    @endforeach
 </x-layout>
