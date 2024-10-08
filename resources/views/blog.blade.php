f2256e3cbb5edc839955fa962f03ae471a74d3db
Raulro03 <raulromeraortiz@gmail.com>
2024-10-08 17:04:21 +0200

    PASO 7 pedir desde una base de datos los datos para verlo en blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,6 +1,7 @@
 <x-layout meta-title="Blog" meta-description="Descripcion de la pagina de Blog">
     <h1>Blog</h1>
     @foreach($posts as $post)
-    <h2>{{ $post['title'] }}</h2>
+    <h2>{{ $post->title }}</h2> <!--Flecha porque al pedir datos de una base de datos lo devuelve como objeto entonces
+    pasamos de $post['title'] que serua para una array a lo otro que es para objeto-->
     @endforeach
 </x-layout>
