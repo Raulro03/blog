011fbf732da988dbf31d0edab466307d0f771f6d
Raulro03 <raulromeraortiz@gmail.com>
2024-10-09 16:42:30 +0200

    Paso 9 y medio Crear Modelo Post para buscar en la tabla posts

---
+++ app/Models/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    //protected $table = 'articles'; Nombre por defecto de la tabla a buscar si la borro buscara Post en mayuscula y minuscula
+}
