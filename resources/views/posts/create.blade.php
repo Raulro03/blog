38803f846d59a885506c2cbd7d258e56b9c29612
Raulro03 <raulromeraortiz@gmail.com>
2024-10-11 16:17:32 +0200

    Paso 14 VALIDACIONES Y MEMORIA EN LOS FORMULARIOS

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,17 +1,26 @@
 <x-layout meta-title="Create a new Post" meta-description="Create a new Post">
 
     <h1> Create a new Post </h1>
+
     <form method="POST" action="{{ route('posts.store') }}">
         @csrf
         <label>
             Title<br />
-            <input type="text" name="title">
+            <input type="text" name="title" value="{{old('title')}}" >
+            @error('title')
+            <br />
+            <small style='color: red'>{{$message}}</small>
+            @enderror
         </label>
         <br />
         <label>
 
             Body <br />
-            <textarea name="body"></textarea>
+            <textarea name="body">{{old("body")}}</textarea>
+            @error('body')
+            <br />
+            <small style='color: red'>{{$message}}</small>
+            @enderror
         </label>
         <br />
         <label>
