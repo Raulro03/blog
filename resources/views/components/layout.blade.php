261692d71a4327f425424011c8bc98ac9c8e0f9d
Raulro03 <raulromeraortiz@gmail.com>
2024-10-16 17:18:13 +0200

    Paso 21 Vista movil , modo oscuro y modificar una plantilla

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,7 +10,7 @@
 
 
 </head>
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+<body class="flex h-screen flex-col bg-slate-100 selection:bg-sky-600 selection:text-sky-50 dark:bg-slate-950">
 <x-partials.navigation/>
 
 @session('status')
@@ -20,16 +20,9 @@
 @endsession
 
 
-{{ $slot }}
-@isset($sidebar)
-
-    <div id="sidebar">
-        <h3>Sidebar</h3>
-
-        {{ $sidebar }}
-
-    </div>
-@endisset
+    <main class="flex-1 p-4">
+     {{ $slot }}
+    </main>
 </body>
 </html>
 
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,8 @@
 <x-layout meta-title="Contacto" meta-description="Descripcion de la pagina de Contacto">
-    <h1>Contacto</h1>
+<div class="mx-auto mt-4 max-w-6x1">
+    <h1 class=" mt-4 mb-8 text-center font-serif text-4x1 font-extrabold text-sky-600 md:text-5x1">
+        Contacto
+    </h1>
+</div>
 
 </x-layout>
