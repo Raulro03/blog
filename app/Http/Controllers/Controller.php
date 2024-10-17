ebe8dd62defb11b201dc19ed8fb6ec73e0e4ca51
Raulro03 <raulromeraortiz@gmail.com>
2024-10-17 16:59:47 +0200

    Paso 24 autentificacion para modificar o borrar posts

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Models\Post;
+use Illuminate\Routing\Controller as BaseController;
+
+abstract class Controller extends BaseController
 {
-    //
+    public function __construct(){
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
+    public function __index() {
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\PostController; //Sirve para indicar directorios de los
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::view('/', 'welcome')->name('home');
 Route::view('contacto', 'contact')->name('contact');
 
@@ -14,7 +12,6 @@ Route::resource('blog', PostController::class)
     ->parameters(['blog' => 'post']);
 
 Route::view('nosotros', 'about')->name('about');
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
