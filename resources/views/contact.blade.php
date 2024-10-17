da75561e445ed88c3030d2f7af299a2d98dd6990
Raulro03 <raulromeraortiz@gmail.com>
2024-10-17 15:51:06 +0200

    Paso 22 modificar .blade usando la layout y tailwind

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,6 @@
 <x-layout meta-title="Contacto" meta-description="Descripcion de la pagina de Contacto">
-<div class="mx-auto mt-4 max-w-6x1">
-    <h1 class=" mt-4 mb-8 text-center font-serif text-4x1 font-extrabold text-sky-600 md:text-5x1">
+<div class="mx-auto mt-4 max-w-6xl">
+    <h1 class=" mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
         Contacto
     </h1>
 </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,10 @@
 <x-layout meta-title="Blog" meta-description="Descripcion de la pagina de Blog">
+    <div class="mx-auto mt-4 max-w-6xl">
+        <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
+            Blog
+        </h1>
+    </div>
 
-    <h1>Blog</h1>
     <a href="{{ route('posts.create') }}">Create a new Posts</a>
     @foreach($posts as $post)
         <div style="display: flex; align-items: baseline">
