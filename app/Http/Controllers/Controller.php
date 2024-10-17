8e4b647eaf21acfa63d410b6115457eb1929654e
Raulro03 <raulromeraortiz@gmail.com>
2024-10-17 17:10:56 +0200

    Paso 24 iniciar sesion

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,13 +7,6 @@ use Illuminate\Routing\Controller as BaseController;
 
 abstract class Controller extends BaseController
 {
-    public function __construct(){
-        $this->middleware('auth')->except(['index', 'show']);
-    }
+//
 
-    public function __index() {
-        $posts = Post::all();
-
-        return view('posts.index', compact('posts'));
-    }
 }
