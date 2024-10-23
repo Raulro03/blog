2452ca980b02e0f2c8e1ff24586d24d39e46c320
Raulro03 <raulromeraortiz@gmail.com>
2024-10-23 16:49:48 +0200

    Creado desplegables de guest y auth

--- resources/views/layouts/blog-navigation.blade.php
+++ resources/views/layouts/blog-navigation.blade.php
@@ -238,28 +238,51 @@
                     </button>
                 </div>
             </div>
+            @guest()
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button>
+                                <img
+                                    class="h-6 w-6 rounded-full"
+                                    src="https://ui-avatars.com/api?name=Jorge+Garcia"
+                                    alt="Jorge García"
+                                />
+                            </button>
 
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('profile.edit')">
+                                {{ __('Login') }}
+                            </x-dropdown-link>
+
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <x-dropdown-link :href="route('logout')"
+                                                 onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                    {{ __('Register') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+            @endguest
+            @auth()
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <!--<button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>// Auth::user()->name </div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>-->
-                        <button
-                            class="ml-4 rounded-full text-slate-500 transition-colors hover:text-sky-500 focus:ring-2 focus:ring-slate-200 focus:ring-offset-1"
-                        >
+                        <button>
                             <img
                                 class="h-6 w-6 rounded-full"
                                 src="https://ui-avatars.com/api?name=Jorge+Garcia"
                                 alt="Jorge García"
                             />
                         </button>
+
                     </x-slot>
 
                     <x-slot name="content">
@@ -280,7 +303,7 @@
                     </x-slot>
                 </x-dropdown>
             </div>
-
+            @endauth
         </div>
     </div>
     <div
