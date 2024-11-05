bb86fec4611305f5f71f99a38c663ad72f3273db
Raulro03 <raulromeraortiz@gmail.com>
2024-11-05 20:19:22 +0100

    Fin Ejercicio 5 y inicio ejercicio de personalizar el logo de inicio de sesion y a la hora de editar tambien poder editar el last name

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $fillable = [
         'name',
+        'last_name',
         'email',
         'password',
     ];
